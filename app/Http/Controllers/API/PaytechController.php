3438c873181923879bbe6d617adce3f5456c16e1
Ntemo-Vanguard <ntemovanga@gmail.com>
2025-12-20 13:44:22 +0000

    Second commit IX

--- app/Http/Controllers/API/PaytechController.php
+++ app/Http/Controllers/API/PaytechController.php
@@ -91,12 +91,6 @@ class PaytechController extends Controller
      */
     public function ipn(Request $request)
     {
-        Log::info('IPN HIT RAW', [
-            'headers' => $request->headers->all(),
-            'body'    => $request->getContent(),
-            'all'     => $request->all(),
-        ]);
-        
         $payload = $request->all();
         Log::info('PayTech IPN received', $payload);
 
